d3b9e5586e80f038557c3027f5da1e3971021c91
thosker <thosker@b-hiveinnovations.co.uk>
2020-02-14 14:49:56 +0000

    Continuing to add New Journal Entry page...

--- sql/create_drop.sql
+++ sql/create_drop.sql
@@ -1,5 +1,5 @@
--- Run me on the database for the HarvestHub app using:
---   heroku pg:psql --app harvesthub < create_drop.sql
+-- Run me on the database for the HGMJ app using:
+--   heroku pg:psql --app hgmj < create_drop.sql
 
 -- Remember that PostgreSQL will changes the names of columns, tables, etc
 -- to all lower case unless quotation marks are used.
@@ -7,7 +7,7 @@
 DROP TABLE IF EXISTS JournalEntry;
 CREATE TABLE JournalEntry
 (
-  id INT PRIMARY KEY,
+  id SERIAL PRIMARY KEY,
   painScore INT NOT NULL,
   theTimeStamp INT NOT NULL,
   remarks VARCHAR(999)
