ef53d931dbdc96f6da967a5acd5160bf09d627f4
tomhosker <tomdothosker@gmail.com>
2021-11-12 12:30:48 +0000

    Added option to specify month and year.

--- python/monthly_graph_maker.py
+++ python/monthly_graph_maker.py
@@ -7,6 +7,7 @@ instructions.
 """
 
 # Standard imports.
+import sys
 import time
 from datetime import datetime
 
@@ -19,30 +20,40 @@ from graph_maker import GraphMaker, MONTHS_IN_A_YEAR, MONTH_NAMES
 
 class MonthlyGraphMaker(GraphMaker):
     """ The class in question. """
-    def __init__(self, show_graph=False):
-        GraphMaker.__init__(self, show_graph=show_graph)
-        self.current_month_no = self.get_current_month_no()
-        self.current_year = self.get_current_year()
+    def __init__(self, month_no=None, year=None, show_graph=False):
+        super().__init__(show_graph=show_graph)
+        self.current_month_no = self.get_current_month_no(month_no)
+        self.current_year = self.get_current_year(year)
         self.prev_month_no = deincrement_month(self.current_month_no)
         self.prev_year = self.get_prev_year()
-        self.current_timestamp = month_and_year_to_epoch(
-                                     self.current_month_no,
-                                     self.current_year)
-        self.prev_timestamp = month_and_year_to_epoch(self.prev_month_no,
-                                                      self.prev_year)
-        self.title = ("Summary for "+MONTH_NAMES[self.prev_month_no-1]+" "+
-                      str(self.prev_year))
-        self.filename = (MONTH_NAMES[self.prev_month_no-1].lower()+
-                         str(self.prev_year)+".png")
+        self.current_timestamp = \
+            month_and_year_to_epoch(
+                self.current_month_no,
+                self.current_year
+            )
+        self.prev_timestamp = \
+            month_and_year_to_epoch(self.prev_month_no, self.prev_year)
+        self.title = (
+            "Summary for "+MONTH_NAMES[self.prev_month_no-1]+" "+
+            str(self.prev_year)
+        )
+        self.filename = (
+            MONTH_NAMES[self.prev_month_no-1].lower()+
+            str(self.prev_year)+".png"
+        )
 
-    def get_current_month_no(self):
+    def get_current_month_no(self, month_no):
         """ Ronseal. """
+        if month_no:
+            return month_no
         now = datetime.now()
         result = now.month
         return result
 
-    def get_current_year(self):
+    def get_current_year(self, year):
         """ Ronseal. """
+        if year:
+            return year
         now = datetime.now()
         result = now.year
         return result
@@ -57,8 +68,10 @@ class MonthlyGraphMaker(GraphMaker):
 
     def print_months_and_years(self):
         """ A debugging method. """
-        print("Current: "+str(self.current_month_no)+", "+
-              str(self.current_year))
+        print(
+            "Current: "+str(self.current_month_no)+", "+
+            str(self.current_year)
+        )
         print("Prev: "+str(self.prev_month_no)+", "+str(self.prev_year))
 
 ####################
@@ -84,12 +97,33 @@ def month_and_year_to_epoch(month_no, year):
     result = int(time.mktime(time.strptime(date_and_time, pattern)))
     return result
 
+def print_help():
+    """ Print a help message when the user inputs illegal arguments. """
+    print(
+        "The correct syntax is:\n\n"+
+        "    python3 monthly_graph_maker.py [MONTH NUMBER] [YEAR]\n\n"+
+        "Where the month number and year are optional."
+    )
+
 ###################
 # RUN AND WRAP UP #
 ###################
 
 def run():
-    mgm = MonthlyGraphMaker()
+    try:
+        if len(sys.argv) >= 3:
+            mgm = \
+                MonthlyGraphMaker(
+                   month_no=int(sys.argv[1]),
+                   year=int(sys.argv[2])
+                )
+        elif len(sys.argv) == 2:
+            mgm = MonthlyGraphMaker(month_no=int(sys.argv[1]))
+        else:
+            mgm = MonthlyGraphMaker()
+    except ValueError:
+        print_help()
+        return
     mgm.make_graph()
 
 if __name__ == "__main__":
