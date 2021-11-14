861e760245bb60d762d4d2c04d208000bff0a8a0
tomhosker <tomdothosker@gmail.com>
2021-11-14 19:17:17 +0000

    Debugging 'Not found' issue...

--- routes/journal.js
+++ routes/journal.js
@@ -16,6 +16,7 @@ const finaliser = new Finaliser();
 
 // Return the page for the whole journal.
 router.get("/", function (req, res, next) {
+console.log("HERRO!");
     scraper.fetchJournal(req, res);
 });
 
